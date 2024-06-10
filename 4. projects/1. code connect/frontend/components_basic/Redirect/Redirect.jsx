4629430f7577c0acf8198e312d19a01c3f598c41
SashrikGupta <sashrikgupta@gmail.com>
2024-06-10 13:05:14 +0530

    ok

--- 4. projects/1. code connect/frontend/components_basic/Redirect/Redirect.jsx
+++ 4. projects/1. code connect/frontend/components_basic/Redirect/Redirect.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Card from '../Card/Card'
 export default function Redirect() {
   return (
    <div className='h-[90vh] w-[100vw] flex justify-center items-center'>
