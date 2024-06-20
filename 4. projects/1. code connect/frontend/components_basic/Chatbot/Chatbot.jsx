66dafb8505f1e87b8f11b907bd6fcb27a3932b64
SashrikGupta <sashrikgupta@gmail.com>
2024-06-20 21:55:04 +0530

    chat bot intigration

--- 4. projects/1. code connect/frontend/components_basic/Chatbot/Chatbot.jsx
+++ 4. projects/1. code connect/frontend/components_basic/Chatbot/Chatbot.jsx
@@ -29,7 +29,7 @@ const Chatbot = () => {
     const userMessage = { name: "User", message: inputValue };
     setMessages([...messages, userMessage]);
      document.getElementById('oj').value = ''
-    fetch('http://127.0.0.1:5000/predict', {
+    fetch('https://chatbot-ydma.onrender.com/predict', {
       method: 'POST',
       body: JSON.stringify({ message: inputValue }),
       mode: 'cors',
